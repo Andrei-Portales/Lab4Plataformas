a9eaee88c21eb06e4ee742a140d80660489b1612
Andrei-Portales <30614183+Andrei-Portales@users.noreply.github.com>
2020-04-26 19:02:00 -0600

    Initial commit

---
+++ app/src/main/java/com/portales/lab4plataformas/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package com.portales.lab4plataformas.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
